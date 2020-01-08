6a3c3167f59429d283137324f21e9b5c93f70f6c
Edebo <akandeadebowale0@gmail.com>
2020-01-08 09:32:27 +0100

    adjusting compound interest formular

--- compound.js
+++ compound.js
@@ -1,11 +1,12 @@
-const principal = 50000;
-const rate = 5;
-const time = 3;
-let amount, compoundInterest;
-
-amount = principal * (1 + rate / 100) ** time;
+const principal = 5000;
+const rate = 0.04;
+const time = 6;
+const number = 4;
+let amount, compoundInterest, period, base;
+base = period = time * number;
+amount = (principal * Math.pow(1 + rate / number, period)).toFixed(2);
 
 //computing compound interest annually to two decimal place
-compoundInterest = (amount - principal).toFixed(2);
+compoundInterest = amount - principal;
 
-console.log(`Compound Interest is ${compoundInterest}`);
+console.log(`Compound Interest is ${compoundInterest},${amount}`);
