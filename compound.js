9574de615165fa256f7b8fe7d325fef227e4fa6f
Edebo <akandeadebowale0@gmail.com>
2020-01-08 09:35:34 +0100

    rounding to 2 decimal places

--- compound.js
+++ compound.js
@@ -1,12 +1,13 @@
 const principal = 5000;
-const rate = 0.04;
-const time = 6;
-const number = 4;
+const rate = 0.05;
+const time = 10;
+const number = 12;
 let amount, compoundInterest, period, base;
-base = period = time * number;
+
+period = time * number;
 amount = (principal * Math.pow(1 + rate / number, period)).toFixed(2);
 
 //computing compound interest annually to two decimal place
-compoundInterest = amount - principal;
+compoundInterest = (amount - principal).toFixed(2);
 
 console.log(`Compound Interest is ${compoundInterest},${amount}`);
